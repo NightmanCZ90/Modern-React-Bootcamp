37c647a5a77d809ce22d01608612ed07dbe12c1e
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-21 01:24:22 +0200

    conditionals in JSX

---
+++ Section 2/conditionals.js
@@ -0,0 +1,23 @@
+function getNum(){
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
+  render(){
+    const num = getNum();
+    return (
+      <div>
+        <h1>Your number is: {num}</h1>
+        <p>{num === 7 ? 'Congrats!' : 'Unlucky'}</p>
+        {
+          num === 7
+          ? <img src="https://media.giphy.com/media/S3Ot3hZ5bcy8o/giphy.gif" />
+          : null
+        }
+        {num === 7 && <img src="https://media.giphy.com/media/DffShiJ47fPqM/giphy.gif" />}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
