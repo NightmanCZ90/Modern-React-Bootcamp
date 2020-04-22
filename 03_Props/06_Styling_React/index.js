e49d6f9a156f085bdccbbf3ada0bccf24976a684
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-23 01:28:58 +0200

    styling React

---
+++ 03_Props/06_Styling_React/index.js
@@ -0,0 +1,21 @@
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Slot Machines!</h1>
+        <Machine 
+          s1="❤"
+          s2="👌"
+          s3="✔"
+        />
+        <Machine 
+          s1="👌"
+          s2="👌"
+          s3="👌"
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
