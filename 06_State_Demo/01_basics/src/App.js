bf35c90d72474d6bd1131a8fafb60170481108d5
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-25 22:17:24 +0200

    set state

--- 06_State_Demo/01_basics/src/App.js
+++ 06_State_Demo/01_basics/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Game from './Game';
 import Demo from './Demo';
+import Rando from './Rando';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Game color="purple" />
-      <Demo animal="Bobcat" food="Pineapple" />
+      <Rando maxNum={7} />
     </div>
   );
 }
