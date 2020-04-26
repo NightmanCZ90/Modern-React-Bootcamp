ff304fdda04040f98f9c1931a2848243bb31d088
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-26 15:28:36 +0200

    correct way to mutate state

--- 08_State_Patterns/state_patterns/src/App.js
+++ 08_State_Patterns/state_patterns/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ScoreKeeper from './ScoreKeeper';
+import IconList from './IconList';
 
 function App() {
   return (
     <div className="App">
-      <ScoreKeeper />
+      <IconList />
     </div>
   );
 }
