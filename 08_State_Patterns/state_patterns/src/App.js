9ce968422dcf379be35728db0062b7bca28d7d1d
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-26 15:59:36 +0200

    Lottory Ball component

--- 08_State_Patterns/state_patterns/src/App.js
+++ 08_State_Patterns/state_patterns/src/App.js
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import ScoreKeeper from './ScoreKeeper';
 import IconList from './IconList';
+import Ball from './Ball';
 
 function App() {
   return (
     <div className="App">
-      <IconList />
+      <Ball num={8} />
+      <Ball num={1} />
+      <Ball num={23} />
+      <Ball num={45} />
     </div>
   );
 }
