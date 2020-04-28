6ba074b5e98a79965d13b3cc416f216ff904c5f9
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-28 20:44:32 +0200

    introduce key

--- 09_React_Events/react_events/src/NumberList.js
+++ 09_React_Events/react_events/src/NumberList.js
@@ -14,7 +14,9 @@ class NumberList extends Component {
     }))
   }
   render(){
-    let nums = this.state.nums.map(n => <NumberItem value={n} remove={this.remove} />);
+    let nums = this.state.nums.map(n => (
+    <NumberItem key={n} value={n} remove={this.remove} />
+    ));
     return(
       <div>
         <h1>Number List</h1>
