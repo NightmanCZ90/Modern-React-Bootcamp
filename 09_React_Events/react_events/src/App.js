01b7021a1ed5b5311f8b8d60b7831a5cee830f49
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-28 14:46:30 +0200

    events, binding this

---
+++ 09_React_Events/react_events/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WiseSquare from './WiseSquare';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Events!</h1>
+      <WiseSquare />
+    </div>
+  );
+}
+
+export default App;
