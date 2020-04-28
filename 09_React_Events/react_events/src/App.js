2c408af93282aba87231e5cb33645c73e3828c6c
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-28 15:11:43 +0200

    binding with arguments

--- 09_React_Events/react_events/src/App.js
+++ 09_React_Events/react_events/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WiseSquare from './WiseSquare';
+import ButtonList from './ButtonList';
 
 function App() {
   return (
     <div className="App">
       <h1>React Events!</h1>
-      <WiseSquare />
+      <ButtonList />
     </div>
   );
 }
