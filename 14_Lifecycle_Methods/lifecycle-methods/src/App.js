3e41cfff0a34dbdf1004b63edb7f14f6e14878dc
NightmanCZ90 <novak.jan90@gmail.com>
2020-05-06 23:02:58 +0200

    load data with async functions

--- 14_Lifecycle_Methods/lifecycle-methods/src/App.js
+++ 14_Lifecycle_Methods/lifecycle-methods/src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Timer from './Timer';
 import ZenQuote from './ZenQuote';
+import GithubUserInfo from './GithubUserInfo';
 
 function App() {
   return (
     <div className="App">
-      <ZenQuote />
+      <GithubUserInfo username='NightmanCZ90' />
     </div>
   );
 }
