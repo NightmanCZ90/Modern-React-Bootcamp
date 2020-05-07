817d254d53e0b7949fcf33347031c93fc9bd5a38
NightmanCZ90 <novak.jan90@gmail.com>
2020-05-07 14:48:53 +0200

    add final functions

--- 14_Lifecycle_Methods/lifecycle-methods/src/Deck.js
+++ 14_Lifecycle_Methods/lifecycle-methods/src/Deck.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react'
+import Card from './Card'
 import axios from 'axios'
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck'
 
 class Deck extends Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       deckID: ''
+       deck: '',
+       cards: []
     }
+    this.getCard = this.getCard.bind(this)
   }
   
-  componentDidMount() {
-    const url = 'https://deckofcardsapi.com/api/deck/new/shuffle'
+  async componentDidMount() {
+    const url = `${API_BASE_URL}/new/shuffle`
     axios.get(url).then(response => {
       console.log(response)
       this.setState({
-        deckID: response.data.deck_id
+        deck: response.data,
       })
     })
   }
+  async getCard(){
+    try {
+      const url = `${API_BASE_URL}/${this.state.deck.deck_id}/draw/`
+      let cardRes = await axios.get(url)
+      if(!cardRes.data.success) {
+        throw new Error("No card remaining")
+      }
+      console.log(cardRes.data)
+      let card = cardRes.data.cards[0]
+      this.setState(st => ({
+        cards: [...st.cards, {
+          id: card.code,
+          image: card.image,
+          name: `${card.value} of ${card.suit}`
+        }]
+      }))
+    } catch (err) {
+      alert(err)
+    }
+  }
+
   render() {
+    const cards = this.state.cards.map(c => (
+      <Card key={c.id} name={c.name} image={c.image} />
+    ))
     return (
       <div>
-        
+        <button onClick={this.getCard} >Draw a card</button>
+        {cards}
       </div>
     )
   }
