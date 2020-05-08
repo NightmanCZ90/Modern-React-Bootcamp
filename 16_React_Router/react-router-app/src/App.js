16c99869a4356feef2d38a417826a0c5dba37a54
NightmanCZ90 <novak.jan90@gmail.com>
2020-05-08 16:05:27 +0200

    Switch, exact, Link Component

--- 16_React_Router/react-router-app/src/App.js
+++ 16_React_Router/react-router-app/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Route, Switch, Link} from 'react-router-dom'
+import About from './About'
 import Dog from './Dog'
+import Contact from './Contact'
 
 function App() {
   return (
     <div className="App">
-      <Route path="/dog" component={Dog} />
+      <nav className="App-nav">
+        <Link to="/">About</Link>
+        <Link to="/dog">Dog</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/dog" component={Dog} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
