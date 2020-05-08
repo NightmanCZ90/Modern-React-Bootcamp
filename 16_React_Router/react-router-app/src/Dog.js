a6e5c16b8ad7286afe29282cf46f2e0e2ad1285d
NightmanCZ90 <novak.jan90@gmail.com>
2020-05-08 15:42:08 +0200

    add '/dog' route + component

---
+++ 16_React_Router/react-router-app/src/Dog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Dog extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
+      </div>
+    )
+  }
+}
+
+export default Dog
