f4378dabfc1fff872c9d9f10b085145de34c3b17
NightmanCZ90 <novak.jan90@gmail.com>
2020-05-08 22:41:35 +0200

    404 page added

--- 17_React_Router_Patterns/router-patterns/src/App.js
+++ 17_React_Router_Patterns/router-patterns/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Food from './Food';
 import Meal from './Meal'
 
 function App() {
   return (
     <div className="App">
-      <Route 
-        exact 
-        path="/food/:name" 
-        // render={(routeProps) => <Food name={routeProps.match.params.name} stuff={routeProps} />} 
-        render={(routeProps) => <Food {...routeProps} />} 
-      />
-      <Route 
-        exact 
-        path="/food/:foodName/drink/:drinkName" 
-        component={Meal} 
-      />
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home</h1>} />
+        <Route 
+          exact 
+          path="/food/:name" 
+          // render={(routeProps) => <Food name={routeProps.match.params.name} stuff={routeProps} />} 
+          render={(routeProps) => <Food {...routeProps} />} 
+        />
+        <Route 
+          exact 
+          path="/food/:foodName/drink/:drinkName" 
+          component={Meal} 
+        />
+        <Route render={() => <h1>ERROR NOT FOUND</h1>} />
+      </Switch>
     </div>
   );
 }
