876da45d44b684f66e3d2aa51662a30d45b80689
NightmanCZ90 <novak.jan90@gmail.com>
2020-08-01 00:14:07 +0200

    add palette name form

--- 18_Massice_Color_Project/colors-app/src/App.js
+++ 18_Massice_Color_Project/colors-app/src/App.js
@@ -31,8 +31,14 @@ class App extends Component {
         <Route 
           exact 
           path='/palette/new' 
-          render={routeProps => <NewPaletteForm savePalette={this.savePalette}  {...routeProps}
-        />} />
+          render={routeProps => (
+            <NewPaletteForm 
+              savePalette={this.savePalette} 
+              palettes={this.state.palettes} 
+              {...routeProps}
+            />
+          )} 
+        />
         <Route 
           exact
           path='/palette/:paletteId/:colorId' 
