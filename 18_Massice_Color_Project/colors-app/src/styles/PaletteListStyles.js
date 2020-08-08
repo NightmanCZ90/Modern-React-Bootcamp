009f083925b382d931f48854559ea0e5ac04752b
NightmanCZ90 <novak.jan90@gmail.com>
2020-08-08 16:01:46 +0200

    make PaletteList responsive

--- 18_Massice_Color_Project/colors-app/src/styles/PaletteListStyles.js
+++ 18_Massice_Color_Project/colors-app/src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
   root: {
     backgroundColor: 'blue',
@@ -11,7 +13,13 @@ export default {
     display: 'flex',
     alignItems: 'flex-start',
     flexDirection: 'column',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%'
+    },
+    [sizes.down('xs')]: {
+      width: '75%'
+    }
   },
   nav: {
     display: 'flex',
@@ -27,7 +35,14 @@ export default {
     boxSizing: 'border-box',
     width: '100%',
     display: 'grid',
-    gridTemplateColumns: 'repeat(3,30%)',
-    gridGap: '5%'
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)'
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1rem'
+    }
   }
 }
